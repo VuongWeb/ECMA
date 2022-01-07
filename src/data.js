fc58ae03cf77d8b6e18579b661560f711822e082
VuongWeb <vuonglvph15121@fpt.edu.vn>
2022-01-07 13:37:37 +0700

    Update css

---
+++ src/data.js
@@ -0,0 +1,18 @@
+const listMenu = ["Trang chủ", "Tuyển sinh", "Chương trình đào tạo", "Góc sinh viên", "Tuyển dụng"];
+const listPost = [
+    {
+        img: "https://picsum.photos/300/200",
+        title: "Vinh danh 295 sinh viên Fpoly học kỳ Spring 2018",
+        content: "Với sự tham gia của các của rất trong các chuyên gia trong nhiều lĩnh vực ,kinh doanh,chương trình đã thu hút đông đảo sinh viên Fpoy tham dự"
+    },
+    {
+        img: "https://picsum.photos/300/200",
+        title: "Vinh danh 295 sinh viên Fpoly học kỳ Spring 2018",
+        content: "Với sự tham gia của các của rất trong các chuyên gia trong nhiều lĩnh vực ,kinh doanh,chương trình đã thu hút đông đảo sinh viên Fpoy tham dự"
+    },
+    {
+        img: "https://picsum.photos/300/200",
+        title: "Vinh danh 295 sinh viên Fpoly học kỳ Spring 2018",
+        content: "Với sự tham gia của các của rất trong các chuyên gia trong nhiều lĩnh vực ,kinh doanh,chương trình đã thu hút đông đảo sinh viên Fpoy tham dự"
+    },
+]
